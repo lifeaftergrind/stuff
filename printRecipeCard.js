8c2f03b47148037f3346371fa2f2aa04a3199bbd
lifeaftergrind <153671378+lifeaftergrind@users.noreply.github.com>
2023-12-13 04:02:07 +0800

    Update printRecipeCard.js

--- printRecipeCard.js
+++ printRecipeCard.js
@@ -34,17 +34,4 @@ function printRecipeCard() {
   printWindow.print();
 }
 
-function toggleRecipeImage() {
-  const recipeImage = document.querySelector('.recipe-image');
-  const recipeMeta = document.querySelector('.recipe-meta');
-  const recipeActions = document.querySelector('.recipe-actions');
-  recipeImage.classList.toggle('hidden'); // Toggles the "hidden" class
-  recipeMeta.classList.toggle('hidden');
-  recipeActions.classList.toggle('hidden');
-}
-
-const toggleButton = document.querySelector('.recipe-action-button.toggle-image');
-toggleButton.addEventListener('click', toggleRecipeImage);
-
-
 
